01689bef8dbf70217392d99c64b2a5e3e3e6552c
Thomas Csizmadia <shadowfax.tdc86@gmail.com>
2026-02-23 21:03:23 +1300

    try making a nginx proxy

--- backend/src/server.jl
+++ backend/src/server.jl
@@ -4,6 +4,9 @@ using Farey
 @get "/" function()
     "hello world!"
 end
+@get "/echo" function(req)
+    return queryparams(req)
+end
 
 @get "/farey" function ()
     "$(ContinuedFraction(30//1323))"
@@ -11,13 +14,13 @@ end
 
 const PORT = parse(Int, get(ENV, "PORT", "8080"))
 
-F2 = FreeGroup((;:f=>:F, :g=>:G))
-f, g = GPC.gens(F2)
-F, G = GPC.gens(F2) .|> inv
+# F2 = FreeGroup((;:f=>:F, :g=>:G))
+# f, g = GPC.gens(F2)
+# F, G = GPC.gens(F2) .|> inv
 
-F2ab = FreeGroup((;:a=>:A, :a=>:B))
-a, b = GPC.gens(F2ab)
-A, B = GPC.gens(F2ab) .|> inv
+# F2ab = FreeGroup((;:a=>:A, :a=>:B))
+# a, b = GPC.gens(F2ab)
+# A, B = GPC.gens(F2ab) .|> inv
 
 
 println("Starting Julia server on port $(PORT) …")
